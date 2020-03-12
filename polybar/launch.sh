02b290cce037aa46cdc6daee1db7ff107048b6dd
William Tang <u1169036@utah.edu>
2020-03-11 19:28:21 -0600

    Update i3 & polybar setting

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch
-polybar top &
-polybar bottom &
-
+#polybar top &
+#polybar bottom &
+polybar example
 echo "Bars launched..."
