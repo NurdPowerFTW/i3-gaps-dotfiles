44a4712b01fb65ca17fb2c38d176ed95170f7bb1
William Tang <u1169036@utah.edu>
2020-03-13 19:44:36 -0600

    Update several configs

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,8 +6,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch
-#polybar top &
-#polybar bottom &
-polybar example
-echo "Bars launched..."
+export DEFAULT_NETWORK_INTERFACE=$(ip route | grep '^default' | awk '{print $5}' | head -n1)
+
+# Launch bar
+polybar top_bar &
+
+echo "Bar launched..."
